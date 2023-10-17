22d0d5b45e4b164a430acc09b472eb0c4b28af3d
PIPER <akinlabi@gmail.com>
2023-10-18 00:32:10 +0100

    ddd

--- app/Http/Controllers/TransController.php
+++ app/Http/Controllers/TransController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Advert;
 use App\Models\RequestFund;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
 
@@ -18,16 +19,18 @@ public function allrequest()
 }
 public function alladvert()
 {
+    $user=User::where('username', Auth::user()->username)->first();
 
-    if (Auth::user()->plan ==null){
+    if ($user->plan ==null){
         $msg="Kindly Subscribe to any Membership plan before any post";
         Alert::info('Subscribe', $msg);
 
         return redirect('plan');
-    }
+    }else {
 
         $advert = Advert::where('username', Auth::user()->username)->get();
         return view('ads.advert', compact('advert'));
+    }
 
 }
 }
