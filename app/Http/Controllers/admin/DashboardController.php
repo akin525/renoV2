01e13aa78d13cc4d52c9aaefce35167cf4a314cf
PIPER <akinlabi@gmail.com>
2023-10-18 11:54:28 +0100

    ddd

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -41,8 +41,35 @@ public function dashboard(Request $request)
         $bil2 = bill_payment::get();
         $bill=bill_payment::sum('amount');
         $lock=bill_payment::sum('discountamount');
-        $tran = 0;
-        $pa = 0;
+        $resellerURL = 'https://integration.mcd.5starcompany.com.ng/api/reseller/';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $resellerURL . 'me',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('service' => 'balance'),
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: mcd_key_75rq4][oyfu545eyuriup1q2yue4poxe3jfd'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+//                                                        return $response;
+        $data = json_decode($response, true);
+        $success = $data["success"];
+        $tran = $data["data"]["wallet"];
+        $pa = $data["data"]["commission"];
 
         $today = Carbon::now()->format('Y-m-d');
 
