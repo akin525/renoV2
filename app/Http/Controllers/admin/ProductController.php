e8e16bf12ab26a499b808e50224854eb1fe179ea
PIPER <akinlabi@gmail.com>
2023-11-29 16:35:50 +0100

    approve

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -105,12 +105,15 @@ public function edit(Request $request)
         'id' => 'required',
         'tamount' => 'required',
         'ramount' => 'required',
+        'pamount' => 'required',
         'name' => 'required',
     ]);
     $pro=data::where('id', $request->id)->first();
     $pro->plan=$request->name;
     $pro->tamount=$request->tamount;
     $pro->ramount=$request->ramount;
+    $pro->api_amount=$request->pamount;
+
     $pro->save();
     return response()->json([
         'status'=>'success',
