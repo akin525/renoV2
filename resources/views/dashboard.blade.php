645cf955d6854ed5c6e1b8a23028643483db5b34
ToSHIBA <akinlabisamson15@yahoo.com>
2024-01-08 23:15:11 +0100

    ads

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,9 @@
 <script src="{{ asset('js/Chart.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
 {{--@livewireChartsScripts--}}
 <script>
     $(document).ready(function() {
