5c7f0e5420e922da1c46d3b66b2c4d64d2d2aa4d
akina <akinlabisamson15@gmail.com>
2024-12-26 20:05:07 +0100

    signup p

--- resources/views/email/verify1.blade.php
+++ resources/views/email/verify1.blade.php
@@ -1,6 +1,6 @@
 <p>Hello,</p>
 <p>Please use the following code to verify your email:</p>
-<h1>{{ $token1['name'] }}</h1>
+<h1>{{json_decode( $token1['name']) }}</h1>
 <h1>{{ $token1['token'] }}</h1>
 {{--<h1>{{ $token1['email'] }}</h1>--}}
 <p>This code is valid for 10 minutes.</p>
